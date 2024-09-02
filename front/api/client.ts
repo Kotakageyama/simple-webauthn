8ce454078a8a365b76564bc5313b62f996cbd945
Kotakageyama <kageyama.kota@hrbrain.co.jp>
2024-09-02 18:12:48 +0900

    feat: Backend URL修正

--- front/api/client.ts
+++ front/api/client.ts
@@ -3,7 +3,7 @@ import createClient from "openapi-fetch";
 import type { paths } from "./api";
 
 const apiClient = createClient<paths>({
-	baseUrl: process.env.BACKEND_URL || "http://localhost:3000",
+    baseUrl: process.env.BACKEND_URL || "http://localhost:8080",
 });
 
 export default apiClient;
